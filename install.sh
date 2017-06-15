83417152ca3d73d8c2633b506e89fdd538923edb
Vasilis Charalampakis <bmpak04@gmail.com>
2017-06-15 13:42:10 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install git struff
+brew install git
+brew install bash-completion
+
+# Clone dotfiles
+git clone https://github.com/charbgr/dotfiles.git
+mv -v ./dotfiles/* ~/
+mv -v ./dotfiles/.* ~/
