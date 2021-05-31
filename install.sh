ab66e72634a85859a79caa2e9b2847e2c66c5ba0
Vasilis Charalampakis <bmpak04@gmail.com>
2021-05-31 16:30:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ brew install bash-completion
 
 # Install Oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # General stuff
 brew cask install java
