f4e2694d5303570e76e280c3193167e001b647fe
Vasilis Charalampakis <bmpak04@gmail.com>
2021-05-31 15:41:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@
 brew install git
 brew install bash-completion
 
+# Git completion for zsh
+mkdir -p ~/.zsh
+cd ~/.zsh
+
+# Download the scripts
+curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o _git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+
 # General stuff
 brew cask install java
 brew install kotlin
